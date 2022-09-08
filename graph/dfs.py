217f8ba3b8559be67bf325913012e5ae4b8c35dd
nkarl <21993921+nkarl@users.noreply.github.com>
2022-09-07 19:30:38 -0700

    Cleaned up the dfs template.

--- graph/dfs.py
+++ graph/dfs.py
@@ -32,17 +32,22 @@ def add_edge(graph, u, v):
     pass
 
 
-if __name__ == "__main__":
-    # creates empty graph
-    g1 = defaultdict(list)
+# creates empty graph
+g1 = defaultdict(list)
 
-    # init the graph with vertices
-    add_edge(g1, 0, 1)
-    add_edge(g1, 0, 2)
-    add_edge(g1, 1, 2)
-    add_edge(g1, 2, 0)
-    add_edge(g1, 2, 3)
-    add_edge(g1, 3, 3)
+# init the graph with vertices
+add_edge(g1, 0, 1)
+add_edge(g1, 0, 2)
+add_edge(g1, 1, 2)
+add_edge(g1, 2, 0)
+add_edge(g1, 2, 3)
+add_edge(g1, 3, 3)
 
-    # run dfs on the graph
-    dfs(g1, 2)
+
+print("Graph:")
+for k, v in g1.items():
+    print(f"\tnode: {k}, adj: {v}")
+
+# run dfs on the graph
+print("Path from 2:", end='\n\t')
+dfs(g1, 2)
