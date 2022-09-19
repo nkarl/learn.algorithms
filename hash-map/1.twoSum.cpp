c0102cc97c512793ca51a5fe656f47d5756d3f16
nkarl <21993921+nkarl@users.noreply.github.com>
2022-09-19 16:42:04 -0700

    Added solution for hashmap.1.twoSum.

--- hash-map/1.twoSum.cpp
+++ hash-map/1.twoSum.cpp
@@ -1,11 +1,24 @@
 #include "../utils.hpp"
 #include <map>
+using std::map;
 
 vector<int> twoSum(vector<int> &nums, int target) {
   /*
    * Could be a greedy algorithm here. But it is a good chance to
    * practice dynamic programming here.
    */
+  // auto ret = new vector<int>();
+  map<int, int> seen = {};
+  for (int i = 0; i < nums.size(); i++) {
+    if (seen.find(nums[i]) != seen.end()) {
+      return {i, seen[nums[i]]};
+    }
+    else {
+        seen[target - nums[i]] = i;
+    }
+  }
+  // In case there is no solution, we'll just return null
+  return {};
 }
 
 /**
@@ -14,9 +27,8 @@ vector<int> twoSum(vector<int> &nums, int target) {
 int main(int argc, char *argv[]) {
   vector<int> nums = {1, 2, 3, 4, 5, 6};
   myPrint(nums); // before
-  /*
-   * Add solution function(s) here.
-   */
+  auto c = twoSum(nums, 7);
   myPrint(nums); // after
+  myPrint(c);
   return 0;
 }
