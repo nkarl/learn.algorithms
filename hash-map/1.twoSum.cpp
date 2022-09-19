f21847c7c44d5d0f93447e85b714166e930e1505
nkarl <21993921+nkarl@users.noreply.github.com>
2022-09-19 15:37:45 -0700

    Added a hash-map problem; 1.twoSum.

---
+++ hash-map/1.twoSum.cpp
@@ -0,0 +1,22 @@
+#include "../utils.hpp"
+#include <map>
+
+vector<int> twoSum(vector<int> &nums, int target) {
+  /*
+   * Could be a greedy algorithm here. But it is a good chance to
+   * practice dynamic programming here.
+   */
+}
+
+/**
+ * MAIN ENTRY.
+ */
+int main(int argc, char *argv[]) {
+  vector<int> nums = {1, 2, 3, 4, 5, 6};
+  myPrint(nums); // before
+  /*
+   * Add solution function(s) here.
+   */
+  myPrint(nums); // after
+  return 0;
+}
