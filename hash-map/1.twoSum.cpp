8920e07f53a405689f34ae9228e92b182ad48912
nkarl <21993921+nkarl@users.noreply.github.com>
2022-09-20 15:32:00 -0700

    Flipped the binary conditional statement for the hashmap.1.twosum problem.

--- hash-map/1.twoSum.cpp
+++ hash-map/1.twoSum.cpp
@@ -1,16 +1,33 @@
 #include "../utils.hpp"
 #include <map>
+
 using std::map;
 
 vector<int> twoSum(vector<int> &nums, int target) {
-  map<int, int> seen = {};
+
+  map<int, int> dict = {}; // Creates a new dictionary.
+
   for (int i = 0; i < nums.size(); i++) {
+    /*
+     * Let candidate be element at i.
+     */
     int candidate = nums[i];
-    if (seen.find(candidate) != seen.end()) {
-      return {seen[candidate], i};
-    }
-    else {
-        seen[target - candidate] = i;
+
+    /*
+     * Binary check if the candidate is already in <dict>.
+     */
+    if (dict.find(candidate) == dict.end()) {
+      /*
+       * If true, memorizes the difference (target - candidate) into
+       * the dictionary with i as value.
+       */
+      dict[target - candidate] = i;
+    } else {
+      /*
+       * Otherwise, returns a 2-vector of the candiate and i right away. (the
+       * two elements summing up to target)
+       */
+      return {dict[candidate], i};
     }
   }
   // In case there is no solution, we'll just return empty vector.
