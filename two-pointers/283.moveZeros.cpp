9b0870b51b263c8b1fe233b45e3596968b09bc9d
nkarl <21993921+nkarl@users.noreply.github.com>
2022-09-22 16:27:59 -0700

    Updated twopointers.283.moveZeros; added explanation to the solution.

--- two-pointers/283.moveZeros.cpp
+++ two-pointers/283.moveZeros.cpp
@@ -2,11 +2,36 @@
 
 using std::swap;
 
+void moveZeroesNOUT(vector<int> &nums) {
+  /*
+   * Input : vector nums
+   * Output: in-place swapping so no output.
+   *
+   * REASONING:
+   * - We use two pointers.
+   * - The first pointer c tracks every iteration of the loop (upperbound is
+   *   N-1).
+   * - The second pointer l does not increment unless the current element c is
+   *   non-zero. In other words, if nums[c] != c, we swap nums[c] with nums[l]
+   *   and then increment l. Here, we can see that l is a slow moving pointer
+   *   that tracks the last non-zero element.
+   * - For this algorithm, c is always increasing, and l only increase when a
+   *   condition is met. If nums[c] is zero, then l is also guaranteed to be
+   *   pointing at a zero.
+   */
+  for (int l = 0, c = 0; c < nums.size(); ++c) {
+    if (nums[c] != 0) {
+      swap(nums[l], nums[c]);
+      ++l;
+    }
+  }
+}
+
 void moveZeroes(vector<int> &nums) {
   for (int l = 0, c = 0; c < nums.size(); ++c) {
-    cout << "for="  << c << ";l=" << l << endl;
-    cout << "nums[" << c << "]==" << nums[c] << "; "
-         << "nums[" << l << "]==" << nums[l];
+    cout << "for c="<< c << ";\tl=" << l << endl;
+    cout << " nums[" << c << "]==" << nums[c] << "; "
+         << " nums[" << l << "]==" << nums[l];
     if (nums[c] != 0) {
       cout << "\tswapping" << endl;
       swap(nums[l], nums[c]);
@@ -25,8 +50,9 @@ void moveZeroes(vector<int> &nums) {
  * MAIN ENTRY.
  */
 int main(int argc, char *argv[]) {
-  vector<int> nums = {1, 0, 2, 0, 3};
+  vector<int> nums = {1, 1, 0, 1, 1, 0, 1};
   myPrint(nums); // before
+  cout << endl;
   moveZeroes(nums);
   myPrint(nums); // after
   return 0;
