e30905b654b8004b35062f4a5e91e8fa61775a82
nkarl <21993921+nkarl@users.noreply.github.com>
2022-09-22 14:59:11 -0700

    Updated hashmap.1.twoSum; streamlined the solution and provided a much more generalized description.

--- hash-map/1.twoSum.cpp
+++ hash-map/1.twoSum.cpp
@@ -4,28 +4,29 @@
 using std::map;
 
 vector<int> twoSum(vector<int> &nums, int target) {
+  // create a new dictionary to store unique parts.
+  map<int, int> dict = {};
 
-  map<int, int> seen = {}; // Creates a new dictionary.
-
-  for (int i = 0; i < nums.size(); i++) {
-    auto candidate = nums[i];
-    auto diff = target - candidate;
-
-    // Find diff.
-    //  if not found, add candidate and its index to dictionary.
-    if (seen.find(diff) == seen.end()) {
-      seen[candidate] = i;
-      cout << "NOT FOUND:" << endl;
-      for (auto i : seen) {
-        cout << "\t\tkey: diff=" << i.first << "\tval: i=" << i.second << endl;
-      }
-    }
-    //  if found, return indices of both diff and candidate.
-    else {
-      return {seen[diff], i};
+  /*
+   * Reasoning:
+   * - Every element is a possible part of the target, where
+   *   the target is composed by exactly two parts at some
+   *   index i and j:
+   *      target = a + b
+   *
+   * - Thus, for every p_i=nums[i], go find its other part p_j.
+   *    - If p_j not found, insert p_i and i as key-value pair into dict.
+   *    - If p_j found, return i and j, where j is and the index saved at dict[p_j].
+   */
+  for (int i = 0; i < nums.size(); ++i) {
+    int a = target - nums[i];
+    int b = nums[i];
+    if (dict.find(a) == dict.end()) {
+      dict[b] = i;
+    } else {
+      return {dict[a], i};
     }
   }
-  // In case there is no solution, we'll just return empty vector.
   return {};
 }
 
