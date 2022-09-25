e85f81e43f50f8e155c74da9996cd30f6f77885f
nkarl <21993921+nkarl@users.noreply.github.com>
2022-09-25 03:59:12 -0700

    Added solution for twoPointers.26.removeDuplicatesFromSortedArray.

--- two-pointers/26.removeDuplicatesFromSortedArray.cpp
+++ two-pointers/26.removeDuplicatesFromSortedArray.cpp
@@ -1,41 +1,40 @@
 #include "../utils.hpp"
 
-using std::swap;
-
-int removeElement(vector<int> &nums, int val) {
-  int k=0;
-  for (int l = 0, c = 0; c < nums.size(); ++c) {
-    cout << "for=" << c << "; l=" << l << endl;
-    cout << "nums[" << c << "]==" << nums[c] << "; "
-         << "nums[" << l << "]==" << nums[l];
-    if (nums[c] != val) {
-      cout << "\tswapping" << endl;
-      swap(nums[l], nums[c]);
-      cout << "\t\t";
-      myPrint(nums);
-      ++l;
-    } else {
-      cout << endl << "\t\t";
-      myPrint(nums);
+int removeDuplicates(vector<int> &nums) {
+    int k = 1;
+    for (int c = 1; c < nums.size(); ++c) {
+        cout << "for=" << c << "; k=" << k << endl;
+        cout << "nums[" << c << "]==" << nums[c] << "; "
+             << "nums[" << k << "]==" << nums[k];
+        if (nums[c - 1] != nums[c]) {
+            cout << "\tswapping" << endl;
+            nums[k] = nums[c];
+            cout << "\t\t";
+            myPrint(nums);
+            ++k;
+        }
+        else {
+            cout << endl
+                 << "\t\t";
+            myPrint(nums);
+        }
+        cout << endl;
     }
-    cout << endl;
-    k = l;
-  }
-  return nums.size() - (nums.size() - k);
+    return k;
 }
 
 /**
  * MAIN ENTRY.
  */
 int main(int argc, char *argv[]) {
-  vector<int> nums = {1, 0, 2, 0, 3};
-  int val = 3;
-  myPrint(nums); // before
-  int c = removeElement(nums, val);
-  myPrint(nums); // after
-  for (int i=0; i < c; ++i) {
-      cout << nums[i] << ' ';
-  }
-  cout << endl;
-  return 0;
+    vector<int> nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+    int         val  = 3;
+    myPrint(nums);  // before
+    int c = removeDuplicates(nums);
+    myPrint(nums);  // after
+    for (int i = 0; i < c; ++i) {
+        cout << nums[i] << ' ';
+    }
+    cout << endl;
+    return 0;
 }
