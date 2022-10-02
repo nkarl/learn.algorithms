af9c1df0a1de03e05f771ada8ad5fcafbbb02b08
nkarl <21993921+nkarl@users.noreply.github.com>
2022-10-02 13:02:51 -0700

    Redone the 14.longestCommonPrefix in divide conquer with commentary.

--- divide-conquer/14.longestCommonPrefix.cpp
+++ divide-conquer/14.longestCommonPrefix.cpp
@@ -1,41 +1,51 @@
 #include "../utils.hpp"
 using std::min; using std::string;
 
-/*
- * Complexity: O(N) Time, O(NlogN) Space.
- */
 
-/**
- * Finds the common prefix (substring) of two strings.
- */
-string commonPrefix(string left, string right) {
-    int shortest = min(left.size(), right.size());
-    // Confirms the substr(i, shortest) is in both left and right.
-    for (int i=0; i < shortest; i++) {
-        if (left[i] != right[i])
-            return left.substr(0, i);
+string getCommonPrefixOf(string s1, string s2) {
+    /*
+     * There are two cases we need to look at:
+     *  1. either one is a prefix of the other. In this case we simply
+     *      return the substring as the range from 0 to the shorter
+     *      string.
+     *
+     *  2. The other case is when the prefix is shorter than both s1 and s2.
+     *      In this case we must iterate and check every character from 0
+     *      to the shorter value. If there is a different char at i, we
+     *      can return the prefix immediately.
+     */
+    int shorter = min(s1.length(), s2.length());
+    for (int i=0; i < shorter; ++i) {
+        if (s1[i] != s2[i]) return s1.substr(0, i);
     }
-    return left.substr(0, shortest);
+    return s1.substr(0, shorter);
 }
 
-/**
- * Find the longest common prefix from a vector of strings.
- */
-string dfs(vector<string> &strs, int l, int r) {
-    if (l == r) return strs[l];     // base case.
-    else {
-        int    m        = (l+r)/2;
-        string lcpLeft  = dfs(strs, l  , m);
-        string lcpRight = dfs(strs, m+1, r);
-        return commonPrefix(lcpLeft, lcpRight);
-    }
+string dfs(vector<string> strs, int lo, int hi) {
+    /*
+     * Assuming the vector contains only two strings. We compare them
+     * directly.
+     *
+     * Now what do we do when the size of vector > 2?
+     * We can use divide and conquer to break the vector down into chunks
+     * small enough that we can use the function above. Notice that the rank
+     * of the vector is just an array of indices. We can use something similar
+     * to binary search to get push elements pairwise on the stack and
+     * then apply the function above to solve recursively. We can use DFS
+     * since it uses an implicit stack via recursion and we don't need to
+     * worry about keeping a stack ourselves.
+     *
+     * We also need a base case to terminate the recursive calls.
+     */
+    if (lo == hi) return strs[lo];
+    int m = lo+(hi-lo)/2;
+    string s1= dfs(strs, lo, m);
+    string s2= dfs(strs, m+1, hi);
+    return getCommonPrefixOf(s1, s2);
 }
 
-/**
- * Public interface that takes a vector of strings.
- */
-string longestCommonPrefix(vector<string> &strs) {
-    if (strs.size() == 0) return "";
+
+string longestCommonPrefix(vector<string> strs) {
     return dfs(strs, 0, strs.size()-1);
 }
 
@@ -43,7 +53,9 @@ string longestCommonPrefix(vector<string> &strs) {
  * MAIN ENTRY.
  */
 int main(int argc, char *argv[]) {
-    vector<string> strs   = {"flow", "flower", "flight"};
+    vector<string> vec1 = {"flow", "flower", "flight"};
+    vector<string> vec2 = {"aaaa", "aaab", "aaac", "aabc"};
+    vector<string> strs = vec2;
     string         prefix = longestCommonPrefix(strs);
     cout << prefix << endl;
     return 0;
