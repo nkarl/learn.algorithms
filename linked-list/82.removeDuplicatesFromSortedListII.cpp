ad4ca6c290c7b818de74b7ffc9795eccab476a0d
nkarl <21993921+nkarl@users.noreply.github.com>
2022-10-06 13:53:12 -0700

    Added solution to linked-list/82.removeDuplicatesFromSortedListII.

--- linked-list/82.removeDuplicatesFromSortedListII.cpp
+++ linked-list/82.removeDuplicatesFromSortedListII.cpp
@@ -3,15 +3,36 @@
 using std::string;
 
 ListNode *deleteDuplicates(ListNode *head) {
-    if (!head) return head;
+    if (!head) {
+        return head;
+    }
 
-    ListNode *parent = head, *child = parent->next;
-    while (child) {
-        child = (parent->val == child->val)
-                    ? parent->next = child->next
-                    : parent       = parent->next;
+    int sentinel = head->val;
+    // If head is found duplicated
+    if (head->next->val == sentinel) {
+        while (head->next && head->next->val == sentinel) {
+            head = head->next;
+        }
+        // Since head node is still the one which was duplicated calling recursion on next Node and updating current head
+        head = deleteDuplicates(head->next);
+    }
+
+    // If head isn't duplicated
+    else {
+        // Leaving current head preserved and calling recursion from the next Node of list
+        head->next = deleteDuplicates(head->next);
     }
     return head;
+
+    //if (!head) return head;
+
+    //ListNode *parent = head, *child = parent->next;
+    //while (child) {
+        //child = (parent->val == child->val)
+                    //? parent->next = child->next
+                    //: parent       = parent->next;
+    //}
+    //return head;
 }
 
 /**
