76a1e5ca91a6b79061e1ab43c2dc7840d0b73456
nkarl <21993921+nkarl@users.noreply.github.com>
2022-10-06 05:42:50 -0700

    Moved the linked list defintions to utils.

--- utils.cpp
+++ utils.cpp
@@ -1,5 +1,6 @@
 #include "utils.hpp"
 
+/* array/vector */
 void myPrint(vector<int> &nums) {
   for (auto x : nums) {
     cout << x << " ";
@@ -8,3 +9,22 @@ void myPrint(vector<int> &nums) {
 }
 
 
+/* linked list */
+void addNode(ListNode *head, int x) {
+    ListNode *node = head;
+    while (node->next != nullptr) node = node->next;
+    node->next = new ListNode(x);
+}
+
+std::string isEmptyNode(ListNode *node) {
+    return (node != nullptr) ? "NotEmpty" : "Empty";
+}
+
+void printList(ListNode *head) {
+    ListNode *node = head;
+    for (; node->next != nullptr;) {
+        cout << node->val << " ";
+        node = node->next;
+    }
+    cout << endl;
+}
--- utils.hpp
+++ utils.hpp
@@ -10,4 +10,16 @@ using std::vector;
 
 void myPrint(vector<int> &nums);
 
+struct ListNode {
+    int       val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) { }
+    ListNode(int x) : val(x), next(nullptr) { }
+    ListNode(int x, ListNode *next) : val(x), next(next) { }
+};
+
+void addNode(ListNode *head, int x); 
+std::string isEmptyNode(ListNode *node);
+void printList(ListNode *head);
+
 #endif
