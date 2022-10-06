6508a65de5e161365f72f97476ebd24d9d4c5273
nkarl <21993921+nkarl@users.noreply.github.com>
2022-10-06 05:55:09 -0700

    Updated linked-list/83.removeDuplicatesFromSortedList.

--- linked-list/83.removeDuplicatesFromSortedList.cpp
+++ linked-list/83.removeDuplicatesFromSortedList.cpp
@@ -2,60 +2,33 @@
 
 using std::string;
 
-struct ListNode {
-    int       val;
-    ListNode *next;
-    ListNode() : val(0), next(nullptr) { }
-    ListNode(int x) : val(x), next(nullptr) { }
-    ListNode(int x, ListNode *next) : val(x), next(next) { }
-};
-
-void addNode(ListNode *head, int x) {
-    ListNode *node = head;
-    while (node->next != nullptr) node = node->next;
-    node->next = new ListNode(x);
-}
-
-string isEmptyNode(ListNode *node) {
-    return (node != nullptr) ? "NotEmpty" : "Empty";
-}
-
 ListNode *deleteDuplicates(ListNode *head) {
     if (head == nullptr) return head;
 
-    ListNode *curr = head;
-    while (curr && curr->next) {
-        if (curr->next->val == curr->val)
-            curr->next = curr->next->next;
-        else
-            curr = curr->next;
+    ListNode *parent = head, *child = parent->next;
+    while (child) {
+        child = (child->val == parent->val)
+                    ? parent->next = child->next
+                    : parent       = parent->next;
+        // if (child->val == parent->val)
+        // child = parent->next = child->next;
+        // else
+        // child = parent = parent->next;
     }
     return head;
 }
 
-void printList(ListNode *head) {
-    ListNode *node = head;
-    for (; node->next != nullptr;) {
-        cout << node->val << " ";
-        node = node->next;
-    }
-    cout << endl;
-}
 /**
  * MAIN ENTRY.
  */
 int main(int argc, char *argv[]) {
     ListNode *head = new ListNode(0);
-    ListNode *node = head;
-    addNode(head, 1);
-    addNode(head, 1);
-    addNode(head, 1);
-    addNode(head, 2);
-    addNode(head, 3);
-    addNode(head, 3);
+    vector<int> list = {1, 1, 1, 2, 3, 3};
+    addNodesFromList(head, list);
 
     printList(head);
     head = deleteDuplicates(head);
     printList(head);
+
     return 0;
 }
