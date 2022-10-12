d6016420b623086b10aca1810ee3c5551e456b34
nkarl <21993921+nkarl@users.noreply.github.com>
2022-10-11 20:14:19 -0700

    Updated.

--- linked-list/82.removeDuplicatesFromSortedListII.cpp
+++ linked-list/82.removeDuplicatesFromSortedListII.cpp
@@ -3,9 +3,7 @@
 using std::string;
 
 ListNode *deleteDuplicates(ListNode *head) {
-    if (!head) {
-        return head;
-    }
+    if (!head) return head;
 
     int sentinel = head->val;
     // If head is found duplicated
@@ -13,33 +11,24 @@ ListNode *deleteDuplicates(ListNode *head) {
         while (head->next && head->next->val == sentinel) {
             head = head->next;
         }
-        // Since head node is still the one which was duplicated calling recursion on next Node and updating current head
+        // Since head node is still the one which was duplicated calling
+        // recursion on next Node and updating current head
         head = deleteDuplicates(head->next);
     }
-
     // If head isn't duplicated
     else {
-        // Leaving current head preserved and calling recursion from the next Node of list
+        // Leaving current head preserved and calling recursion from the next
+        // Node of list
         head->next = deleteDuplicates(head->next);
     }
     return head;
-
-    //if (!head) return head;
-
-    //ListNode *parent = head, *child = parent->next;
-    //while (child) {
-        //child = (parent->val == child->val)
-                    //? parent->next = child->next
-                    //: parent       = parent->next;
-    //}
-    //return head;
 }
 
 /**
  * MAIN ENTRY.
  */
 int main(int argc, char *argv[]) {
-    ListNode *head = new ListNode(0);
+    ListNode   *head = new ListNode(0);
     vector<int> list = {1, 1, 1, 2, 3, 3};
     addNodesFromList(head, list);
 
