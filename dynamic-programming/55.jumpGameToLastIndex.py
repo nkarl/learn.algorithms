6d44dad6fded21bb0cf40700c7e227a02d7918e2
nkarl <21993921+nkarl@users.noreply.github.com>
2023-02-23 12:27:59 -0800

    Updated.

---
+++ dynamic-programming/55.jumpGameToLastIndex.py
@@ -0,0 +1,18 @@
+
+def canJumpFrom(position: int, nums: list[int]) -> bool:
+    if (position == len(nums) -1):
+        return True
+    farthest = min(position + nums[position],
+                   len(nums) -1)
+    for nextJump in range(position +1, farthest +1):
+        if canJumpFrom(nextJump, nums):
+            return True
+    return False
+
+
+
+def canJump(nums: list[int]):
+    return canJumpFrom(0, nums)
+
+
+nums = [2,3,1,1,4]
