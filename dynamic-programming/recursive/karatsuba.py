89315abc8976988fb22a20d20266713f20c68f4f
nkarl <21993921+nkarl@users.noreply.github.com>
2023-08-21 19:25:49 -0700

    Updated.

--- dynamic-programming/recursive/karatsuba.py
+++ dynamic-programming/recursive/karatsuba.py
@@ -9,7 +9,7 @@ def recursive_multiply(x: str, y: str, n: int) -> int:
     bc = recursive_multiply(b, c, s)
     bd = recursive_multiply(b, d, s)
 
-    return (10 ** n) * ac + (10 ** (n//2)) * (ad + bc) + bd
+    return (10**n) * ac + (10 ** (n // 2)) * (ad + bc) + bd
     # return ((10 ** (n // 2)) * int(a) + int(b)) * ((10 ** (n // 2)) * int(c) + int(d))
 
 
@@ -24,4 +24,4 @@ def karatsuba(x: str, y: str, n: int) -> int:
     pq = recursive_multiply(a + b, c + d, s)
     adbc = pq - ac - bd
 
-    return (10 ** n) * ac + (10 ** (n//2)) * (adbc) + bd
+    return (10**n) * ac + (10 ** (n // 2)) * (adbc) + bd
