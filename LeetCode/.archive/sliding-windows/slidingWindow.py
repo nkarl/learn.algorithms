9b91a19ef8e7c68e54e8bc85edcdeb41cefa9bc1
nkarl <21993921+nkarl@users.noreply.github.com>
2023-09-03 13:38:09 -0700

    Updated.

--- LeetCode/.archive/sliding-windows/slidingWindow.py
+++ LeetCode/.archive/sliding-windows/slidingWindow.py
@@ -1,22 +1,29 @@
-s = [3, 5, 2, 1, 7]
-k = 2
+
+# s = [3, 5, 2, 1, 7]
+# k = 2
 
 
-def getMaxSum(arr, k):
-    maxSum = 0
-    windowSum = 0
-    p = 0
+# def getMaxSum(arr, k):
+# maxSum = 0
+# windowSum = 0
+# p = 0
 
-    for i in range(len(arr)):
-        windowSum += arr[i]
+# for i in range(len(arr)):
+# windowSum += arr[i]
 
-        if (i - p + 1) == k:
-            maxSum = max(maxSum, windowSum)
-            windowSum -= arr[p]
-            p += 1
+# if (i - p + 1) == k:
+# maxSum = max(maxSum, windowSum)
+# windowSum -= arr[p]
+# p += 1
 
-    return maxSum
+# return maxSum
 
 
-result = getMaxSum(s, k)
-print(result)
+# result = getMaxSum(s, k)
+# print(result)
+
+s = "123456789123456789"
+
+k = 10
+li = [s[i:i+k] for i in range(len(s) - k + 1)]
+print("\n".join(li))
--- LeetCode/Easy/551. Student Attendance Record I.py
+++ LeetCode/Easy/551. Student Attendance Record I.py
@@ -6,5 +6,16 @@ def reduce(s: str):
     return s.count('A') <= 1 and s.count('LLL') == 0
 
 
-result = reduce(s)
-print(result)
+def reduce2(s: str):
+    absent = 0
+    for i in range(len(s)):
+        absent += 1 if s[i] == 'A' else 0
+        if absent > 1:
+            return False
+        if s[i:i+3] == 'LLL':
+            return False
+    return True
+
+
+result = reduce2(s)
+print(s, result)
