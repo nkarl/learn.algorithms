e2d4b714d22b80fe47491bddf8dd4db002d3398b
nkarl <21993921+nkarl@users.noreply.github.com>
2023-09-03 15:51:50 -0700

    Updated.

--- LeetCode/.archive/sliding-windows/slidingWindow.py
+++ LeetCode/.archive/sliding-windows/slidingWindow.py
@@ -3,27 +3,36 @@
 # k = 2
 
 
-# def getMaxSum(arr, k):
-# maxSum = 0
-# windowSum = 0
-# p = 0
+def getMaxSum(arr, k):
+    maxSum = 0
+    windowSum = 0
+    p = 0
 
-# for i in range(len(arr)):
-# windowSum += arr[i]
+    for i in range(len(arr)):
+        windowSum += arr[i]
 
-# if (i - p + 1) == k:
-# maxSum = max(maxSum, windowSum)
-# windowSum -= arr[p]
-# p += 1
+    if (i - p + 1) == k:
+        maxSum = max(maxSum, windowSum)
+        windowSum -= arr[p]
+        p += 1
 
-# return maxSum
+    return maxSum
 
 
 # result = getMaxSum(s, k)
 # print(result)
 
 s = "123456789123456789"
-
 k = 10
-li = [s[i:i+k] for i in range(len(s) - k + 1)]
-print("\n".join(li))
+
+
+def slidingWindow(s, k):
+    # subs = []
+    # for i in range(len(s) - k + 1):
+    #   subs += [s[i:i+k]]
+    subs = [s[i:i+k] for i in range(len(s) - k + 1)]
+    return "\n".join(subs)
+
+
+result = slidingWindow(s, k)
+print(result)
