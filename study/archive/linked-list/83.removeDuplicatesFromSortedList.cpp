e15e498b0e38d8028eafcf3489e9689cf2e415df
nkarl <21993921+nkarl@users.noreply.github.com>
2023-09-27 09:29:45 -0700

    Updated.

--- study/archive/linked-list/83.removeDuplicatesFromSortedList.cpp
+++ study/archive/linked-list/83.removeDuplicatesFromSortedList.cpp
@@ -5,12 +5,12 @@ using std::string;
 ListNode *deleteDuplicates(ListNode *head) {
     if (!head) return head;
 
-    ListNode *parent = head,
-             *child  = parent->next;
+    ListNode *parent= head,
+             *child = parent->next;
     while (child) {
-        child = (parent->val == child->val)
-                    ? parent->next = child->next
-                    : parent       = parent->next;
+        child= (parent->val == child->val)
+                   ? parent->next= child->next
+                   : parent      = parent->next;
     }
     return head;
 }
@@ -19,12 +19,12 @@ ListNode *deleteDuplicates(ListNode *head) {
  * MAIN ENTRY.
  */
 int main(int argc, char *argv[]) {
-    ListNode   *head = new ListNode(0);
-    vector<int> list = {1, 1, 1, 2, 3, 3};
+    ListNode   *head= new ListNode(0);
+    vector<int> list= {1, 1, 1, 2, 3, 3};
     addNodesFromList(head, list);
 
     printList(head);
-    head = deleteDuplicates(head);
+    head= deleteDuplicates(head);
     printList(head);
 
     return 0;
