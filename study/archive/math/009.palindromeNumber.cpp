2224c9797e7bb4eca4dd562317f686f13c08f987
nkarl <21993921+nkarl@users.noreply.github.com>
2023-10-02 20:48:12 -0700

    Updated.

--- study/archive/math/009.palindromeNumber.cpp
+++ study/archive/math/009.palindromeNumber.cpp
@@ -1,15 +1,27 @@
 #include "../utils.cpp"
 
+bool palindrome(int lo, int hi) {
+    if (lo <= hi) {
+        return lo == hi || lo == hi / 10;
+    }
+    hi = hi * 10 + lo % 10;
+    lo /= 10;
+    printf("lo=%d hi=%d\n", lo, hi);
+    return palindrome(lo, hi);
+}
+
 bool isPalindrome(int x) {
     // edge case: x < 0 or x is modulus of base 10.
     if (x < 0 || (x % 10 == 0 && x != 0)) {
         return false;
     }
 
+    //return palindrome(x, 0);
     int lo= x, hi= 0;
     while (lo > hi) {
         hi= hi * 10 + lo % 10;
         lo/= 10;
+        printf("lo=%d hi=%d\n", lo, hi);
     }
     printf("\tlo=\t%d\thi=\t%d\t", lo, hi);
 
@@ -20,7 +32,7 @@ bool isPalindrome(int x) {
  * MAIN ENTRY.
  */
 int main(int argc, char *argv[]) {
-    vector<int> input= {1, 121, 123, 124421, 15, 36063};
+    vector<int> input= {-10, -121, 1, 121, 123, 124421, 15, 36063};
     for (auto x : input) {
         printf("%d\t isPalindrome() -->%s\n", x, (isPalindrome(x) ? "True" : "False"));
     }
