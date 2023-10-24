5f149f5ada6c8fe04386b6706c23d788760c0719
nkarl <21993921+nkarl@users.noreply.github.com>
2023-10-24 16:33:02 -0700

    vault backup: 2023-10-24 16:33:02

---
+++ study/archive/binary-search/014.longestCommonPrefix.py
@@ -0,0 +1,34 @@
+strs = ["flower", "flow", "flight"]
+
+
+def isLCP(strs, tail) -> bool:
+    prefix = strs[0][:tail]
+    for s in strs:
+        if s.find(prefix) != 0:
+            return False
+    return True
+
+
+def search(strs, lo, hi) -> str:
+    if lo > hi:
+        return strs[0][:hi]
+    m = lo + (hi - lo) // 2
+    print(lo, m, hi)
+    if isLCP(strs, m):
+        return search(strs, m + 1, hi)
+    else:
+        return search(strs, lo, m - 1)
+
+
+def LCP(strs) -> str:
+    if len(strs) == 0:
+        return ""
+    if len(strs) == 1:
+        return strs[0]
+    shortest = len(strs[0])
+    for s in strs:
+        shortest = min(shortest, len(s))
+    return search(strs, 0, shortest)
+
+
+LCP(strs)
